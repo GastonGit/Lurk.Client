aacee38ce389be2326fbc6bfd40fad31ffedb0aa
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-05-05 22:18:58 +0200

    convert Header to functional component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import '../styles/Header.css';
 
-export default class Header extends React.Component<unknown, unknown> {
-    render(): JSX.Element {
-        return (
-            <header className="header">
-                <div className="header__content">
-                    <a
-                        href="https://gastongit.com/lurk"
-                        className="header__link"
-                    >
-                        Lurk
-                    </a>
-                </div>
-            </header>
-        );
-    }
+export default function Header(): JSX.Element {
+    return (
+        <header className="header">
+            <div className="header__content">
+                <a href="https://gastongit.com/lurk" className="header__link">
+                    Lurk
+                </a>
+            </div>
+        </header>
+    );
 }
