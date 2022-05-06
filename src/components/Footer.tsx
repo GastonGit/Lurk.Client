801c9b7e019d405e2397fe4d5e364f99868d3200
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-05-06 13:57:40 +0200

    convert Footer to functional component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import '../styles/Footer.css';
 import { Grid } from '@mui/material';
 
-export default class Footer extends React.Component<unknown, unknown> {
-    constructor(props: unknown) {
-        super(props);
-        this.state = {};
-    }
-
-    render(): JSX.Element {
-        return (
-            <div className="footer">
-                <div className="footer__content">
-                    <Grid container>
-                        <Grid item xs={4} sm={3} md={2}>
-                            <a href="https://github.com/GastonGit/Lurk.Client">
-                                About
-                            </a>
-                        </Grid>
+export default function Footer(): JSX.Element {
+    return (
+        <div className="footer">
+            <div className="footer__content">
+                <Grid container>
+                    <Grid item xs={4} sm={3} md={2}>
+                        <a href="https://github.com/GastonGit/Lurk.Client">
+                            About
+                        </a>
                     </Grid>
-                </div>
+                </Grid>
             </div>
-        );
-    }
+        </div>
+    );
 }
